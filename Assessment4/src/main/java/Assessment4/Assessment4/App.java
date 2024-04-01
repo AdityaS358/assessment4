5fbddb5a8697f7362a8346c53b2442f125015ade
AdityaS358 <130598697+AdityaS358@users.noreply.github.com>
2024-04-01 19:26:03 +0530

    Add files via upload

---
+++ Assessment4/src/main/java/Assessment4/Assessment4/App.java
@@ -0,0 +1,13 @@
+package Assessment4.Assessment4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Assessment4/src/test/java/Assessment4/Assessment4/AppTest.java
@@ -0,0 +1,62 @@
+package Assessment4.Assessment4;
+
+
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+
+public class AppTest {
+	WebDriver driver;
+	
+    @DataProvider(name = "loginData")
+    public Object[][] getLoginData() {
+        return new Object[][]{
+                {"abcd", "1234"},
+                {"xyzz", "1234"},
+        };
+    }
+
+	@Test
+	public void openBrowser() {
+		System.setProperty("webdriver.chrome.driver", "D:\\Selenium Drivers\\Chrome Driver\\chromedriver-win64\\chromedriver.exe");
+		
+		driver = new ChromeDriver();
+		
+		driver.get("https://codenboxautomationlab.com/");
+		
+		System.out.println(driver.getTitle());
+	
+}
+	 @Test(dataProvider = "loginData")
+	    public void loginTest(String username, String password) {
+	        System.setProperty("webdriver.chrome.driver", "D:\\Selenium Drivers\\Chrome Driver\\chromedriver-win64\\chromedriver.exe");
+
+	        driver = new ChromeDriver();
+
+	        driver.get("https://codenboxautomationlab.com/wp-login.php");
+
+	        System.out.println(driver.getTitle());
+
+
+	        WebElement user = driver.findElement(By.name("log"));
+	        user.sendKeys(username);
+
+	        WebElement pass = driver.findElement(By.name("pwd"));
+	        pass.sendKeys(password);
+
+	        WebElement btn = driver.findElement(By.cssSelector("input[type='submit"));
+	        btn.click();
+
+	        System.out.println(driver.findElement(By.xpath("//*[@id='login_error']")).getText());
+	        
+	        driver.quit();
+	    }
+	
+}
+
+
